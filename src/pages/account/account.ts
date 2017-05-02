675ff2201668371e3bfcc2a68169cdfea0d728ae
dThaqif <dyry.17@gmail.com>
2017-05-02 18:50:37 +0800

    Initialize commit

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the Account page.
@@ -12,13 +13,33 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-account',
   templateUrl: 'account.html',
 })
+
 export class AccountPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  base64Image: string;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Account');
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
 
+  }
+  takePhoto(){
+    const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64:
+    this.base64Image = 'data:image/jpeg;base64,' + imageData;
+  }, (err) => {
+    // Handle error
+  });
+
+  }
+  
+
+ionViewDidLoad() {
+  console.log('ionViewDidLoad Account');
+}
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AboutPage } from '../about/about';
-import { SearchPage} from '../search/search';
+import { SearchPage } from '../search/search';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-   
-   aboutPage=AboutPage;
-   searchPage=SearchPage;
+
+  aboutPage = AboutPage;
+  searchPage = SearchPage;
 
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
 
-  }
 }
